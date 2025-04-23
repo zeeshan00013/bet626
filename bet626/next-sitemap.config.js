193f3a67bd4121734f759e2b3bc4b4277482081d
zeeshan00013 <za953042@gmail.com>
2025-04-23 21:50:12 +0500

    sitemap

--- bet626/next-sitemap.config.js
+++ bet626/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://www.bet626game.cc",
+  siteUrl: "https://bet626game.cc",
   generateRobotsTxt: true,
   generateIndexSitemap: false,
   sitemapSize: 7000,
